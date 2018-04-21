283adec5bfc52c7c7afda9e016874d7fcdb823a2
Fernando Fragoso <fernandofragoso@gmail.com>
2018-04-21 10:59:39 -0300

    Fetch candidates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      candidates: []
+    };
+  }
+
+  componentWillMount() {
+    //API URL
+    const url = "http://localhost:3000/candidates/";
+
+    //fetch candidates
+    fetch(url, {
+      method: 'GET'
+    }).then(res => res.json())
+    .then(response => {
+      this._setCandidates(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  
   render() {
     return (
       <div className="App">
-        
+        <ul>
+          {this.state.candidates.map(candidate => <li key={candidate.id}>{candidate.name}</li>)}
+        </ul>
       </div>
     );
+  }
+
+  _setCandidates(candidates) {
+    this.setState({
+      candidates: candidates
+    });
   }
 }
 
